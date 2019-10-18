1c47f5583170b83d4352abcfc11a102131e0dc84
Jim Clingenpeel <no.spam.jac@gmail.com>
2019-10-18 12:00:57 -0400

    wip

---
+++ src/components/Song.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-if="index === 0">{{ title }}</h1>
+    <div v-html="lyricParts[index]"></div>
+    <button v-on:click="index--">&lt;</button>
+    <button v-on:click="index++">&gt;</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Song",
+  data() {
+    return {
+      title: "title here",
+      lyricParts: [
+        "html lyric 1",
+        "<p>html lyric 2</p><p>another line</p>",
+        "html lyric 3"
+      ],
+      index: 0
+    };
+  },
+  methods: {
+    //   lyric() {
+    //       return 'jim';
+    //     // return this.lyricParts[this.index]
+    //   }
+  }
+};
+</script>
+
